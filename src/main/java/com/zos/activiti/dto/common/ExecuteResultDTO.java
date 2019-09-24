01533562e1c664c3300cfc3bb6098d34bca4f754
zos <wangrupeng@foxmail.com>
2019-09-24 18:29:53 +0800

    .

---
+++ src/main/java/com/zos/activiti/dto/common/ExecuteResultDTO.java
@@ -0,0 +1,22 @@
+package com.zos.activiti.dto.common;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * 公共响应信息
+ * 
+ * @author 01Studio
+ *
+ */
+@Data
+@ApiModel(description = "公共响应信息")
+public class ExecuteResultDTO {
+	@ApiModelProperty(value = "响应消息", example = "成功: success, 失败: exception msg")
+	private String msg;
+	@ApiModelProperty(value = "响应数据")
+	private Object obj;
+	@ApiModelProperty(value = "响应结果", example = "成功: 1, 失败: 0")
+	private Integer code;
+}
---
+++ src/main/java/com/zos/activiti/dto/common/Page.java
@@ -0,0 +1,65 @@
+package com.zos.activiti.dto.common;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 分页查询的返回类
+ * 
+ * @author 01Studio
+ *
+ */
+public class Page<T> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6277931081357303827L;
+	
+	private int rowCount = 0;
+
+	private int pageCount = 0;
+
+	private List<T> pageData;
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	public int getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public List<T> getPageData() {
+		return pageData;
+	}
+
+	public void setPageData(List<T> pageData) {
+		this.pageData = pageData;
+	}
+
+	/**
+	 * 根据每页记录数计算页数.
+	 *
+	 * @param pageSize 每页记录数
+	 */
+	public void generatePageCount(int pageSize) {
+
+		if (rowCount == 0 || pageSize == 0) {
+			pageCount = 0;
+		}
+		if ((rowCount % pageSize) > 0) {
+			pageCount = (rowCount / pageSize) + 1;
+		} else {
+			pageCount = rowCount / pageSize;
+		}
+	}
+}
